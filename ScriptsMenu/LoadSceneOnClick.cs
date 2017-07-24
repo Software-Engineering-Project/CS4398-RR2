69f57dd5331797d0a3073a203a443d4a1786b68b
MCSANCHEZ093 <MCSANCHEZ093@GMAIL.COM>
2017-07-24 16:10:33 -0500

    Commit

---
+++ ScriptsMenu/LoadSceneOnClick.cs
@@ -0,0 +1,1 @@
+﻿using UnityEngine;using System.Collections;using UnityEngine.SceneManagement;public class LoadSceneOnClick : MonoBehaviour {	public void LoadByIndex(int sceneIndex)	{		SceneManager.LoadScene (sceneIndex);	}}
---
+++ ScriptsMenu/QuitOnClick.cs
@@ -0,0 +1,1 @@
+﻿using UnityEngine;using System.Collections;public class QuitOnClick : MonoBehaviour {	public void Quit()	{		#if UNITY_EDITOR		UnityEditor.EditorApplication.isPlaying = false;		#else		Application.Quit ();		#endif	}}
